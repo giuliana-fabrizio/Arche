4f403df8fd6939a94628eede6890ee95da2ba1c3
Giuliana Fabrizio <godailfabriziogiuliana@gmail.com>
2025-03-16 22:34:38 +0100

    fix: change active item in nav-tabs

--- admin/catalogue_page.js
+++ admin/catalogue_page.js
@@ -1,4 +1,13 @@
 function changeTabs(dest) {
-    document.getElementById("id_ue").style.display = dest == "ue" ? "block" : "none";
-    document.getElementById("id_user").style.display = dest == "ue" ? "none" : "block";
+    if (dest == "ue") {
+        document.getElementById("id_ue").style.display = "block";
+        document.getElementById("id_ue_tabs").classList.add("active");
+        document.getElementById("id_user").style.display = "none";
+        document.getElementById("id_user_tabs").classList.remove("active");
+    } else {
+        document.getElementById("id_user").style.display = "block";
+        document.getElementById("id_user_tabs").classList.add("active");
+        document.getElementById("id_ue").style.display = "none";
+        document.getElementById("id_ue_tabs").classList.remove("active");
+    }
 }
