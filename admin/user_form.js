6b0bf6048ca638e0e551ead3c02044b051b4fc11
Giuliana Fabrizio <godailfabriziogiuliana@gmail.com>
2025-03-24 19:03:15 +0100

    fix: display ue select

---
+++ admin/user_form.js
@@ -0,0 +1,4 @@
+function changeRole(e) {
+    const value = document.getElementById("id_role").value;
+    document.getElementById("id_container_ues").style.display = value == 1 ? "none" : "block";
+}
