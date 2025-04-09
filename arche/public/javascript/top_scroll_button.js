753f13f17b9346b8470bfe71a970a1fe436bd0a8
Giuliana Fabrizio <godailfabriziogiuliana@gmail.com>
2025-04-09 16:28:31 +0200

    feat: hide top scroll button if window.scrollY <= 50

--- arche/public/javascript/top_scroll_button.js
+++ arche/public/javascript/top_scroll_button.js
@@ -1,3 +1,18 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const btn_scroll = document.getElementById("id_btn_scroll_top");
+
+    function checkScroll() {
+        if (window.scrollY > 50) {
+            btn_scroll.style.display = "block";
+        } else {
+            btn_scroll.style.display = "none";
+        }
+    }
+
+    window.addEventListener("scroll", checkScroll);
+    checkScroll();
+});
+
 function scrollToTop() {
     window.scrollTo({
         top: 0,
