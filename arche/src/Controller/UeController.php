97557ac652deab91fcfb756c764537a67380d8e0
Giuliana Fabrizio <godailfabriziogiuliana@gmail.com>
2025-04-12 19:11:42 +0200

    chore: minor changes

--- arche/src/Controller/UeController.php
+++ arche/src/Controller/UeController.php
@@ -25,6 +25,15 @@ class UeController extends AbstractController {
     }
 
 
+    #[Route('/ajax/delete/post/{id}', name: 'app_ajax_delete_post')]
+    public function deletePost(Request $request) : Response {
+        if($request->isXmlHttpRequest()) {
+            return new JsonResponse(200);
+        }
+        return new JsonResponse(['error' => 'Cet appel doit être effectué via AJAX.'], Response::HTTP_BAD_REQUEST);
+    }
+
+
     #[Route('/choose_ue', name: 'app_ue_choose')]
     public function chooseUe() : Response {
         return $this->render("/home/home.html.twig");
