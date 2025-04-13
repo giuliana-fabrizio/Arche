e8f9940f3d81bf26571bb848334924bba8400eae
Giuliana Fabrizio <godailfabriziogiuliana@gmail.com>
2025-04-13 14:55:53 +0200

    feat: add button to create post

--- arche/public/javascript/content_ue.js
+++ arche/public/javascript/content_ue.js
@@ -3,11 +3,15 @@ function changeTabs(elem, dest) {
         document.getElementById("id_cours").style.display = "block";
         document.getElementById("id_participants").style.display = "none";
 
+        document.getElementById("id_btn_add_post").style.display = "block";
+
         document.getElementById("id_tab_cours").classList.add("active");
         document.getElementById("id_tab_participants").classList.remove("active");
     } else {
         document.getElementById("id_participants").style.display = "block";
         document.getElementById("id_cours").style.display = "none";
+
+        document.getElementById("id_btn_add_post").style.display = "none";
 
         document.getElementById("id_tab_participants").classList.add("active");
         document.getElementById("id_tab_cours").classList.remove("active");
