79a9e7339f49bb57860b1ebe37cd9ede26c77a30
Giuliana Fabrizio <godailfabriziogiuliana@gmail.com>
2025-04-17 15:01:21 +0200

    feat: can login

--- arche/src/Controller/MainController.php
+++ arche/src/Controller/MainController.php
@@ -8,7 +8,19 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class MainController extends AbstractController {
 
-    #[Route('/admin/manage_resource', name: 'app_catalogue')]
+    #[Route('/', name: 'app_home')]
+    public function home() : Response {
+        if (
+            $this->getUser()->getRoles()[0] == "ROLE_ADMIN" ||
+            $this->getUser()->getRoles()[0] == "ROLE_ADMIN_PROFESSEUR"
+        ) {
+            return $this->render("/admin/catalogue.html.twig");
+        }
+        return $this->render("/home/home.html.twig");
+    }
+
+
+    #[Route('/admin/manage_resource', name: 'app_catalogue')] // TODO
     public function manageResource() : Response {
         return $this->render("/admin/catalogue.html.twig");
     }
