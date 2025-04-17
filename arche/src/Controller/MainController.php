a7c97c77365f5297b122ff602491ee853264fbec
Giuliana Fabrizio <godailfabriziogiuliana@gmail.com>
2025-04-17 09:13:37 +0200

    chore: remove login route from MainController

--- arche/src/Controller/MainController.php
+++ arche/src/Controller/MainController.php
@@ -8,12 +8,6 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class MainController extends AbstractController {
 
-    #[Route('/', name: 'app_login')]
-    public function login() : Response {
-        return $this->render("login.html.twig");
-    }
-
-
     #[Route('/admin/manage_resource', name: 'app_catalogue')]
     public function manageResource() : Response {
         return $this->render("/admin/catalogue.html.twig");
